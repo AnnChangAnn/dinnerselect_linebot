25728d6e4902b984630323e821875cbae9aa1307
fatan19928 <fatan19928@hotmail.com.tw>
2020-01-09 23:13:46 +0800

    dbset

--- dbset.py
+++ dbset.py
@@ -7,15 +7,31 @@ import psycopg2
 DATABASE_URL = os.environ['DATABASE_URL']
 #DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a ann-chang-dinnereat').read()[:-1]
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
-create_table_query = '''CREATE TABLE tblfoodlist(
-    seqno serial PRIMARY KEY,
-    foodtype VARCHAR (50) NOT NULL,
-    foodname VARCHAR (50) NOT NULL    );'''
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#cursor = conn.cursor()
+#create_table_query = '''CREATE TABLE tblfoodlist(
+#    seqno serial PRIMARY KEY,
+#    foodtype VARCHAR (50) NOT NULL,
+#    foodname VARCHAR (50) NOT NULL    );'''
+#
+#cursor.execute(create_table_query)
+#conn.commit()
 
-cursor.execute(create_table_query)
+record = [('food', '維力炸醬麵'),('food', '辣炒年糕'),('food', '餛飩乾麵')]
+table_columns = '(foodtype, foodname)'
+postgres_insert_query = f"""INSERT INTO alpaca_training {table_columns} VALUES (%s, %s);"""
+
+#cursor.execute(postgres_insert_query, record)
+cursor.executemany(postgres_insert_query, records)
 conn.commit()
+
+count = cursor.rowcount
+print(count, "Record inserted successfully into tblfoodlist")
+
+postgres_select_query = f"""SELECT * FROM alpaca_training"""
+
+cursor.execute(postgres_select_query)
+cursor.fetchmany()
 #
 #    message = f"恭喜您！ 資料成功建立 tblfoodlist 表單！"
 #    print(message)
