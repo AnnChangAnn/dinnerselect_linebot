825c9e0b74c4e8777da8670b75fe1d56951fbaef
fatan19928 <fatan19928@hotmail.com.tw>
2020-01-09 23:44:34 +0800

    dbset

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def callback():
     app.logger.info("Request body: " + body)
     # handle webhook body
     
-    print(body)
-    
+#    print(body)
+#        Udf831d201cfb768fdef117767f15f0a9
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -43,50 +43,53 @@ def handle_message(event):
     strCheck = str(event.message.text)
     print(strCheck)
     
-    if event.message.text == "晚餐吃啥":
-        strfoodlist = "雞腿便當,咖哩飯,濃厚豚骨拉麵"
-        strfoodlist = "白湯拉麵 豚骨拉麵 泡菜拉麵 墨魚拉麵 海鮮蓋飯 鰻魚蓋飯 豬排蓋飯 天婦羅蓋飯 鱈魚蓋飯 海鮮蓋飯 中華蓋飯 生魚片蓋飯 什錦豆皮壽司 總匯三明治 鍋燒烏龍麵 韓式牛肉湯飯 銅鑼燒 泡芙 天婦羅蕎麥麵 蛋包飯 牛肉燴飯 茶泡飯 烤飯糰 照燒鰤魚 鹽烤鮭魚定食 烤竹莢魚定食 義式什錦湯 紅燒喜知次魚 海鮮炒飯 法式鹹派 蛋包飯 豬肉木耳炒蛋 海鮮義大利麵 蘑菇義大利麵 炸肉餅 炸竹筴魚 什錦煎餅 車輪餅 肉燥飯 紅燒金目鯛魚 燒烤西京味噌鰆魚 鯛姿蒸 蟹肉奶油可樂餅 牛肉蛋包飯 豬排咖哩飯 蟹肉燴炒飯 焢肉燴飯 烏龍麵 薑汁豬肉片 日式漢堡排 大阪壽司 山藥泥麥飯 燒肉井 美式肋排 中式油淋雞 香蒜牛排 蔥抓餅 泰式辣炒蝦 辣炒花蛤 辣炒花枝 辣炒豬 辣炒雞 泰味鳳梨蝦球 香茅辣炒雞肉 肉粽 泰式辣炒海鮮 酸辣叢林燒肉 醉仙牛肉 泰式生菜碎肉 鄉村牛肉 蟳蒸蛋 綜合壽司 關東煮 烤香魚 味噌湯 烤味噌魚 海鮮火鍋 和風沙拉 鮭魚飯團 蒜頭蛤湯 酸筍炒豬肉 炸蝦飯 豚天火燒 海老奶油燒 紅酒燉牛肉 干貝起士燒 牛肉奶油燒 牛蘆筍卷燒 鍋貼 鼎邊趖 泡泡冰 天婦羅 營養三明治 豆簽羹 蝦仁肉圓 鐵板燒 紅油抄手 大餅包小餅 刀削麵 豬肝湯 花枝羹 蚵仔煎 藥燉排骨 牛舌餅 新竹肉圓 貢丸 潤餅 蔥花煎餅 魷魚羹 米粉 潤餅 當歸鴨 蚵仔煎 炒花枝 肉羹 米糕 甜不辣 涼圓 大腸麵線 魷魚羹 魚酥羹 麻辣鴨血 麻辣臭豆腐 炸雞排 滷味 炒米粉 鴨肉羹 魯肉飯 排骨飯 煎餃 雞腿飯 臭豆腐 豪大大雞排 花枝羹 豬血糕 雞肉飯 藍藍路 維力雜醬麵 牛肉麵 鴨肉羹 御飯糰 MOS漢堡 肯德基 頂呱呱 丹丹 刺身 涮羊肉 廣東粥 麻辣鍋 雞絲麵 燒餅 油條 薑母鴨 甜不辣 水餃 可麗餅 小籠包 水餃 水煎包 車輪餅 蚵仔煎 炒米粉 炒羊肉 "
-        strfood = strfoodlist.split( )
-        message = TextSendMessage(text= random.choice(strfood))
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
     
-        #message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
-    elif event.message.text == "吃拉麵嗎":
-        strfoodlist = "鬼金棒,壹之穴,豚人,麵屋輝,半熟堂,東京油組,Soba Shinn & 柑橘,美濃屋,勝王,你回來啦,吉天元,勝千代,涼風奄,小櫻,極匠,蘭丸,鷹流,悠然,麵屋壹慶,雞吉君,雞二,小川,真登,真劍,五之神,誠屋,道樂屋台,羽畠食堂,大和家,麵屋山茶,特濃屋,山嵐拉麵,武藤,旺味麵場,麵屋一燈,花月嵐,凪NAGI,一幻,麵屋武藏,麵屋一騎,小山拉麵,通堂,屯京拉麵,山頭火,霸嗎,双豚,森住康二,一番星,北一家,熊越岳,DUE ITALIAN,太陽番茄拉麵,玩笑亭,博多幸龍,初,一風堂,三冬麵鋪,沾麵玉,豚戈屋台,麵屋一鮫,隱家拉麵,荷麵亭,山形心心拉麵,百八龍拉麵,麵屋秋匠,墨洋拉麵,鳥人拉麵,麵屋武藏神山,一蘭,重熙老麵,麵屋千雲,TSUTA TAIWAN(蔦),三田製麵所,麵SATO SOBA,AJARI拉麵,札幌炎神,濟善老麵,哲麵,麵屋公子,辰拉麵,昕家,23私房拉麵,海貓亭,勝八,大角拉麵,極清拉麵,麵屋一擊,麵屋一虎,不二家拉麵"
-        strfood = strfoodlist.split(",")
+    
+        if event.message.text == "晚餐吃啥":
+            strfoodlist = "雞腿便當,咖哩飯,濃厚豚骨拉麵"
+            strfoodlist = "白湯拉麵 豚骨拉麵 泡菜拉麵 墨魚拉麵 海鮮蓋飯 鰻魚蓋飯 豬排蓋飯 天婦羅蓋飯 鱈魚蓋飯 海鮮蓋飯 中華蓋飯 生魚片蓋飯 什錦豆皮壽司 總匯三明治 鍋燒烏龍麵 韓式牛肉湯飯 銅鑼燒 泡芙 天婦羅蕎麥麵 蛋包飯 牛肉燴飯 茶泡飯 烤飯糰 照燒鰤魚 鹽烤鮭魚定食 烤竹莢魚定食 義式什錦湯 紅燒喜知次魚 海鮮炒飯 法式鹹派 蛋包飯 豬肉木耳炒蛋 海鮮義大利麵 蘑菇義大利麵 炸肉餅 炸竹筴魚 什錦煎餅 車輪餅 肉燥飯 紅燒金目鯛魚 燒烤西京味噌鰆魚 鯛姿蒸 蟹肉奶油可樂餅 牛肉蛋包飯 豬排咖哩飯 蟹肉燴炒飯 焢肉燴飯 烏龍麵 薑汁豬肉片 日式漢堡排 大阪壽司 山藥泥麥飯 燒肉井 美式肋排 中式油淋雞 香蒜牛排 蔥抓餅 泰式辣炒蝦 辣炒花蛤 辣炒花枝 辣炒豬 辣炒雞 泰味鳳梨蝦球 香茅辣炒雞肉 肉粽 泰式辣炒海鮮 酸辣叢林燒肉 醉仙牛肉 泰式生菜碎肉 鄉村牛肉 蟳蒸蛋 綜合壽司 關東煮 烤香魚 味噌湯 烤味噌魚 海鮮火鍋 和風沙拉 鮭魚飯團 蒜頭蛤湯 酸筍炒豬肉 炸蝦飯 豚天火燒 海老奶油燒 紅酒燉牛肉 干貝起士燒 牛肉奶油燒 牛蘆筍卷燒 鍋貼 鼎邊趖 泡泡冰 天婦羅 營養三明治 豆簽羹 蝦仁肉圓 鐵板燒 紅油抄手 大餅包小餅 刀削麵 豬肝湯 花枝羹 蚵仔煎 藥燉排骨 牛舌餅 新竹肉圓 貢丸 潤餅 蔥花煎餅 魷魚羹 米粉 潤餅 當歸鴨 蚵仔煎 炒花枝 肉羹 米糕 甜不辣 涼圓 大腸麵線 魷魚羹 魚酥羹 麻辣鴨血 麻辣臭豆腐 炸雞排 滷味 炒米粉 鴨肉羹 魯肉飯 排骨飯 煎餃 雞腿飯 臭豆腐 豪大大雞排 花枝羹 豬血糕 雞肉飯 藍藍路 維力雜醬麵 牛肉麵 鴨肉羹 御飯糰 MOS漢堡 肯德基 頂呱呱 丹丹 刺身 涮羊肉 廣東粥 麻辣鍋 雞絲麵 燒餅 油條 薑母鴨 甜不辣 水餃 可麗餅 小籠包 水餃 水煎包 車輪餅 蚵仔煎 炒米粉 炒羊肉 "
+            strfood = strfoodlist.split( )
+            message = TextSendMessage(text= random.choice(strfood))
         
-        strreplylist = "好阿！吃,好！ ,我想吃,可以！吃,要不要吃,好！我們去吃,走阿吃,好好好好好！吃,好窩～那我們吃,可哇！吃,行！想吃,吃吃吃吃吃,拉麵吃起來！吃,吃一波"
-        strreply = strreplylist.split(",")
-        message = TextSendMessage(text= random.choice(strreply) + random.choice(strfood) + "!!")
-        
-        line_bot_api.reply_message(event.reply_token, message)
-    elif event.message.text == "好美":
-        message = TextSendMessage(text="哪有你美")
-        line_bot_api.reply_message(event.reply_token, message)
-    elif event.message.text == "中餐吃啥" or event.message.text == "午餐吃啥":
-        message = TextSendMessage(text="沒有 只吃晚餐")
-        line_bot_api.reply_message(event.reply_token, message)
-    elif event.message.text == "早餐吃啥" or event.message.text == "早點吃啥":
-        message = TextSendMessage(text="沒有 只吃晚餐")
-        line_bot_api.reply_message(event.reply_token, message)
-    elif event.message.text == "消夜吃啥" or event.message.text == "宵夜吃啥":
-        message = TextSendMessage(text="沒有 只吃晚餐")
-        line_bot_api.reply_message(event.reply_token, message)
-    elif strCheck.find('晚餐吃啥') >= 0:
-        if strCheck.find('自我介紹') >= 0 :
-            message = TextSendMessage(text="輸入 晚餐吃啥 or 吃拉麵嗎 來獲得良好的建議！")
+            #message = TextSendMessage(text=event.message.text)
             line_bot_api.reply_message(event.reply_token, message)
-    elif strCheck.find('吃') == 0:
-        if strCheck.find('嗎') == len(strCheck) -1:
-            if strCheck.find('晚餐') == -1 and strCheck.find('中餐') == -1 and strCheck.find('早餐') == -1 and strCheck.find('晚飯') == -1 and strCheck.find('午餐') == -1 and strCheck.find('早飯') == -1 and strCheck.find('宵夜') == -1 and strCheck.find('早點') == -1 and strCheck.find('消夜') == -1 and strCheck.find('夜消') == -1 and strCheck.find('夜宵') == -1:
-                message = TextSendMessage(text="不要！只吃拉麵！")
+        elif event.message.text == "吃拉麵嗎":
+            strfoodlist = "鬼金棒,壹之穴,豚人,麵屋輝,半熟堂,東京油組,Soba Shinn & 柑橘,美濃屋,勝王,你回來啦,吉天元,勝千代,涼風奄,小櫻,極匠,蘭丸,鷹流,悠然,麵屋壹慶,雞吉君,雞二,小川,真登,真劍,五之神,誠屋,道樂屋台,羽畠食堂,大和家,麵屋山茶,特濃屋,山嵐拉麵,武藤,旺味麵場,麵屋一燈,花月嵐,凪NAGI,一幻,麵屋武藏,麵屋一騎,小山拉麵,通堂,屯京拉麵,山頭火,霸嗎,双豚,森住康二,一番星,北一家,熊越岳,DUE ITALIAN,太陽番茄拉麵,玩笑亭,博多幸龍,初,一風堂,三冬麵鋪,沾麵玉,豚戈屋台,麵屋一鮫,隱家拉麵,荷麵亭,山形心心拉麵,百八龍拉麵,麵屋秋匠,墨洋拉麵,鳥人拉麵,麵屋武藏神山,一蘭,重熙老麵,麵屋千雲,TSUTA TAIWAN(蔦),三田製麵所,麵SATO SOBA,AJARI拉麵,札幌炎神,濟善老麵,哲麵,麵屋公子,辰拉麵,昕家,23私房拉麵,海貓亭,勝八,大角拉麵,極清拉麵,麵屋一擊,麵屋一虎,不二家拉麵"
+            strfood = strfoodlist.split(",")
+            
+            strreplylist = "好阿！吃,好！ ,我想吃,可以！吃,要不要吃,好！我們去吃,走阿吃,好好好好好！吃,好窩～那我們吃,可哇！吃,行！想吃,吃吃吃吃吃,拉麵吃起來！吃,吃一波"
+            strreply = strreplylist.split(",")
+            message = TextSendMessage(text= random.choice(strreply) + random.choice(strfood) + "!!")
+            
+            line_bot_api.reply_message(event.reply_token, message)
+        elif event.message.text == "好美":
+            message = TextSendMessage(text="哪有你美")
+            line_bot_api.reply_message(event.reply_token, message)
+        elif event.message.text == "中餐吃啥" or event.message.text == "午餐吃啥":
+            message = TextSendMessage(text="沒有 只吃晚餐")
+            line_bot_api.reply_message(event.reply_token, message)
+        elif event.message.text == "早餐吃啥" or event.message.text == "早點吃啥":
+            message = TextSendMessage(text="沒有 只吃晚餐")
+            line_bot_api.reply_message(event.reply_token, message)
+        elif event.message.text == "消夜吃啥" or event.message.text == "宵夜吃啥":
+            message = TextSendMessage(text="沒有 只吃晚餐")
+            line_bot_api.reply_message(event.reply_token, message)
+        elif strCheck.find('晚餐吃啥') >= 0:
+            if strCheck.find('自我介紹') >= 0 :
+                message = TextSendMessage(text="輸入 晚餐吃啥 or 吃拉麵嗎 來獲得良好的建議！")
                 line_bot_api.reply_message(event.reply_token, message)
-            elif strCheck.find('晚餐') >= 0 or strCheck.find('晚飯') >= 0:
-                message = TextSendMessage(text="好阿！要吃什麼？")
-                line_bot_api.reply_message(event.reply_token, message)
-            else:
-                message = TextSendMessage(text="不要！只吃晚餐！")
-                line_bot_api.reply_message(event.reply_token, message)
+        elif strCheck.find('吃') == 0:
+            if strCheck.find('嗎') == len(strCheck) -1:
+                if strCheck.find('晚餐') == -1 and strCheck.find('中餐') == -1 and strCheck.find('早餐') == -1 and strCheck.find('晚飯') == -1 and strCheck.find('午餐') == -1 and strCheck.find('早飯') == -1 and strCheck.find('宵夜') == -1 and strCheck.find('早點') == -1 and strCheck.find('消夜') == -1 and strCheck.find('夜消') == -1 and strCheck.find('夜宵') == -1:
+                    message = TextSendMessage(text="不要！只吃拉麵！")
+                    line_bot_api.reply_message(event.reply_token, message)
+                elif strCheck.find('晚餐') >= 0 or strCheck.find('晚飯') >= 0:
+                    message = TextSendMessage(text="好阿！要吃什麼？")
+                    line_bot_api.reply_message(event.reply_token, message)
+                else:
+                    message = TextSendMessage(text="不要！只吃晚餐！")
+                    line_bot_api.reply_message(event.reply_token, message)
 #    else:
 #        message = TextSendMessage(text="")
 #        line_bot_api.reply_message(event.reply_token, message)
--- db.py
+++ db.py
@@ -1,24 +1,22 @@
+
 import os
 import psycopg2
+
 
 def line_insert_record(record_list):
     DATABASE_URL = os.environ['DATABASE_URL']
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-
-    create_table_query = '''CREATE TABLE tblfoodlist(
-        seqno serial PRIMARY KEY,
-        foodtype VARCHAR (50) NOT NULL,
-        foodname VARCHAR (50) NOT NULL
-    );'''
-
-    cursor.execute(create_table_query)
-    conn.commit()
     
+    table_columns = '(foodtype, foodname)'
+    postgres_insert_query = f"""INSERT INTO tblfoodlist {table_columns} VALUES (%s, %s);"""
+
+    cursor.executemany(postgres_insert_query, record_list)
+    conn.commit()
+
     message = f"恭喜您！ 資料成功建立 tblfoodlist 表單！"
     print(message)
-
     cursor.close()
     conn.close()
 
