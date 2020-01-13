710061adb88b2720f62e2bd8b662c1285587c133
fatan19928 <fatan19928@hotmail.com.tw>
2020-01-13 22:34:36 +0800

    dbset

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def handle_message(event):
 #            line_bot_api.reply_message(event.reply_token, message)
         elif event.message.text == "create table":
             reply = checkfoodlist.line_create_table(event)
-        elif strCheck.find('刪除db資料') >= 0::
+        elif strCheck.find('刪除db資料') >= 0:
             reply = checkfoodlist.line_delete_data(event)
     #    else:
 #        message = TextSendMessage(text="")
