cfb1067c9e20fa7e8710abd73136bb17aa0286df
fatan19928 <fatan19928@hotmail.com.tw>
2020-02-12 01:25:46 +0800

    try fix

--- foodlist/checkfoodlist.py
+++ foodlist/checkfoodlist.py
@@ -193,7 +193,7 @@ def create_message_template(foodtype, txtmain):
 #        pattern = ',["https://"\S*".jpg"'
 
         ActualImages=[]# contains the link for Large original images, type of  image
-        for a in soup.find("img", class_="rg_i Q4LuWd tx8vtf", limit=1):
+        for a in soup.find("img", class_="rg_i Q4LuWd tx8vtf"):
             print(a.get('data-iurl'))
 #            link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
 #            ActualImages.append((link,Type))
