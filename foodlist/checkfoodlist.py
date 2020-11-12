24e27cf5bc3f0feaefb89dd3c91fb261dcaa3241
fatan19928 <fatan19928@hotmail.com.tw>
2020-11-13 00:26:54 +0800

    123

--- foodlist/checkfoodlist.py
+++ foodlist/checkfoodlist.py
@@ -282,7 +282,37 @@ def lineNotifyMessage(event):
         headers = {
             "Authorization": "Bearer " + token,
             "Content-Type" : "application/x-www-form-urlencoded"}
-        payload = {'message': q_string}
+            
+        Location_List = ['臺北市','新北市','桃園市','基隆市']
+        msg = "今日白天天氣\r"
+        msg_weather = ''
+        
+        for i in Location_List[0:]:
+            cwbapi = f"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-E74B046A-CAB4-4570-B4D9-FA3F7F225DC1&locationName={i}&startTime="
+            cwbr = requests.get(cwbapi)
+            print('request success!!')
+        
+            cwbr_dict = cwbr.json()
+            json_string = json.dumps(cwbr_dict)
+            json_format = json.loads(json_string)
+            location = json_format['records']['location'][0]['locationName']
+            weather =     json_format['records']['location'][0]['weatherElement'][0]['time'][0]    ['parameter']['parameterName']
+            MinT =     json_format['records']['location'][0]['weatherElement'][2]['time'][0]    ['parameter']['parameterName']
+            MaxT =     json_format['records']['location'][0]['weatherElement'][4]['time'][0]    ['parameter']['parameterName']
+            pop =     json_format['records']['location'][0]['weatherElement'][1]['time'][0]    ['parameter']['parameterName']
+            #lng = json_format['results'][0]['geometry']['location']['lng']
+            #print(lat)
+            #print(lat, lng)
+            msg_weather = msg_weather + f'''
+            
+        {location}: {weather}
+        氣溫: {MinT}度~{MaxT}度
+        降雨機率: {pop}%'''
+        
+        payload = {'message': msg + msg_weather}
+            
+            
+        #payload = {'message': q_string}
         r = requests.post("https://notify-api.line.me/api/notify", headers = headers,     params =     payload)
         return q_string
         
