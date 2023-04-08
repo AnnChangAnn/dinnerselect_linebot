1a2281e654bd813100c50dafb66f1ae8378d493e
Ann Chang <fatan19928@gmail.com>
2023-04-08 10:00:03 +0800

    add log and timeout

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN',  None))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN',  None), timeout=30)
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET',  None))
 chatGPT_key = os.getenv('AI_APIKEY', None)
 
@@ -121,6 +121,7 @@ def handle_message(event):
             openai.api_key = chatGPT_key
             # 將第5個字元之後的訊息發送給 OpenAI
             prompt = strCheck[4:]
+            app.logger.info("Send request to GPT-3.5")
             response = openai.ChatCompletion.create(
                 model='gpt-3.5-turbo',
                 messages=[
@@ -130,9 +131,10 @@ def handle_message(event):
             )
             # 接收到回覆訊息後，移除換行符號
             reply_msg = response['choices'][0]['message']['content'].replace('\n', '')
+            app.logger.info("respond : " + reply_msg)
 
             message = TextSendMessage(text=reply_msg)
-            line_bot_api.reply_message(event.reply_token, message)
+            line_bot_api.reply_message(event.reply_token, message, timeout=30)
 
         elif strCheck.find('！公告 ') == 0 or strCheck.find('!公告 ') == 0: #geocoding test
             reply = checkfoodlist.lineNotifyAnnounce(event)
