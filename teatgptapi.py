4862c1b0bf5fbeaef980ee39bbad2d49cb5d0c15
Ann Chang <fatan19928@gmail.com>
2023-05-29 21:28:57 +0800

    try to fix bug

--- teatgptapi.py
+++ teatgptapi.py
@@ -1,7 +1,8 @@
 import openai
 import os
 
-openai.api_key = os.getenv('AI_APIKEY', None)
+openai.api_key = 'sk-K !xu.4 kWMEw !ur.3 wP4yBQrXfjduT !n0  BlbkFJT !-4 FExcB !xu.4 BRioP !fu hHqq !xu/6 J'
+#openai.api_key = os.getenv('AI_APIKEY', None)
 
 completion = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
